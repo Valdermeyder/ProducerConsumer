5d84fb93d1fd59873867a54cf8d2ad4465e92b77
Volodymyr Huzar <vguzar@i.ua>
2017-04-26 10:44:30 +0200

    Fixes for CAS solution

--- src/StoreServiceCas.java
+++ src/StoreServiceCas.java
@@ -1,8 +1,5 @@
 import java.util.concurrent.atomic.AtomicInteger;
 
-/**
- * Created by JCW478 on 4/25/2017.
- */
 public class StoreServiceCas implements StoreService {
 
     private AtomicInteger store = new AtomicInteger(0);
@@ -13,7 +10,7 @@ public class StoreServiceCas implements StoreService {
         int val = store.get();
         while (val < 10) {
             if (store.compareAndSet(val, val + 1)) {
-                System.out.println("adding: " + val + 1);
+                System.out.println("value after add: " + (val + 1));
                 break;
             } else {
                 val = store.get();
@@ -30,7 +27,8 @@ public class StoreServiceCas implements StoreService {
         int val = store.get();
         while (val > 0) {
             if (store.compareAndSet(val, val - 1)) {
-                System.out.println("adding: " + (val - 1));
+                System.out.println("value after retrieve: " + (val - 1));
+                return val;
             } else {
                 val = store.get();
             }
